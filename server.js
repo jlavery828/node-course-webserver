5120c726f9d20d80805a1b2373871a1560f855a5
Jason <jlavery828@gmail.com>
2017-07-20 12:16:30 -0400

    Add project page

--- server.js
+++ server.js
@@ -49,6 +49,12 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page'
+  });
+});
+
 app.get('/bad', (req, res) => {
   res.send({
     errorMessage: 'Unable to handle request'
@@ -56,5 +62,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log('Server is up on port ${port}');
+  console.log(`Server is up on port ${port}`);
 });
